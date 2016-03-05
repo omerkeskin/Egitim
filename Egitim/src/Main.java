af2ebc24ff1dff23c8c05d3d0d89d916729b9df9
omerkeskin <omer.keskin.deu@hotmail.com>
2016-03-06 00:05:20 +0200

    test or 1

--- Egitim/src/Main.java
+++ Egitim/src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("test lok 2");
 		System.out.println("test lok 2");
+		
+               System.out.println("test or 2");
 	}
 
 }
