bd8761e7dd48edd59f17d9cc34b6623c39eba461
Omer Keskin <omer.keskin.deu@hotmail.com>
2016-03-06 00:12:24 +0200

    test lok 4

--- Egitim/src/Main.java
+++ Egitim/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test lok 2");
-		System.out.println("test lok 2");
-                System.out.println("test or 2");
+		System.out.println("test lok 4");
+		System.out.println("test lok 4");
+        System.out.println("test lok 4");
 	}
 
 }
