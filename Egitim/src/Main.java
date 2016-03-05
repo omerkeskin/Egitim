3cc0b5074e25656445c30fa137669f14304cf8f7
Omer Keskin <omer.keskin.deu@hotmail.com>
2016-03-05 23:56:36 +0200

    test lok 2

--- Egitim/src/Main.java
+++ Egitim/src/Main.java
@@ -3,8 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test lok 1");
-
+		System.out.println("test lok 2");
+		System.out.println("test lok 2");
 	}
 
 }
